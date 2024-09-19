b4fcc99e15dee3994ff5fc6fd21b73d856e9d414
KoshYana25 <yana.koshelia25@gmail.com>
2024-09-19 18:46:02 +0300

    Adding homework 8.1 Arrays in JS

---
+++ homework_8/task_6.js
@@ -0,0 +1,12 @@
+const numbersList = [1,10,14,2,4,5,43,34];
+
+
+const sortedNumbersList = [...numbersList];
+
+
+sortedNumbersList.sort((a, b) => a - b);
+
+
+console.log("Вихідний масив:", numbersList);
+console.log("Відсортований масив:", sortedNumbersList);
+
